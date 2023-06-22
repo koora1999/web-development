c4a6592c86e0cde3d96b4c44aa32beeed15ac54b
pravalika <pravalikakoora1999@gmail.com>
2023-06-22 12:11:19 +0530

    initialcommit

---
+++ Ecommarcewebsite/script.js
@@ -0,0 +1,17 @@
+
+const bar = document.getElementById("bar");
+const navbar = document.getElementById("navbar");
+const close = document.getElementById("close")
+
+if(bar){
+    bar.addEventListener("click",function(){
+        navbar.classList.add("active")
+    })
+}
+
+if(close){
+    close.addEventListener("click",function(){
+        navbar.classList.remove("active")
+    })
+}
+
